4150ba8a64f4f790ce69eac70d54efd2326ec1d0
githubmao <maoyanbit@foxmail.com>
2018-05-02 15:30:23 +0800

    同比功率，同初始速度，同坡度，上坡行驶速度比较

--- PW.R
+++ PW.R
@@ -9,7 +9,7 @@ kPWFileList <- list.files(pattern = "*.txt")  # 分析数据文件名
 kPWDataName <- gsub(".txt", "", kPWFileList)  # 导入数据集命名
 df.pw <- data.frame()  # 创建用于合并数据的数据集
 
-for(i in 1:length(kPWDataName)){  # 导入数据
+for (i in 1:length(kPWDataName)) {  # 导入数据
   
   kColName <- c("Station", "Speed")
   
@@ -35,13 +35,13 @@ for(i in 1:length(kPWDataName)){  # 导入数据
 df.expectedspeed80pw6.0grade5.0 <- df.pw[df.pw$Grade == "5.0",]
 
 plot.expectedspeed80pw6.0grade5.0 <- ggplot(df.expectedspeed80pw6.0grade5.0,
-                                            aes(x = Station, y = Speed))+
-  geom_line(aes(colour = factor(Axles)), size = 1.2)+
-  geom_hline(yintercept = 80, linetype = "dashed", color = "red", size = 1.0)+
-  scale_x_continuous(limits = c(0, 5000), breaks = seq(0, 5000, 500))+
-  scale_y_continuous(limits = c(0, 85), breaks = seq(0, 85, 10))+
-  scale_colour_hue("轴数")+
-  labs(title = "冲坡速度80km/h, 比功率6.0kW/T，坡度5.0%", x = "位置", y = "速度, km/h")+
+                                            aes(x = Station, y = Speed)) +
+  geom_line(aes(colour = factor(Axles)), size = 1.2) +
+  geom_hline(yintercept = 80, linetype = "dashed", color = "red", size = 1.0) +
+  scale_x_continuous(limits = c(0, 5000), breaks = seq(0, 5000, 500)) +
+  scale_y_continuous(limits = c(0, 85), breaks = seq(0, 85, 10)) +
+  scale_colour_hue("轴数") +
+  labs(title = "冲坡速度80km/h, 比功率6.0kW/T，坡度5.0%", x = "位置", y = "速度, km/h") +
   theme(legend.title = element_text(face = "bold", size = 10),
         legend.text = element_text(face = "bold", size = 10),
         axis.title = element_text(face = "bold", size = 11),
@@ -53,7 +53,7 @@ plot.expectedspeed80pw6.0grade5.0
 
 # 冲破速度80，比功率6.0，坡度5.0%，不同轴数货车稳定速度----
 tmpdf.lastspeedgrade5.0 <- subset(df.expectedspeed80pw6.0grade5.0,
-                                  df.expectedspeed80pw6.0grade5.0$Station%/%1 == 4500)
+                                  df.expectedspeed80pw6.0grade5.0$Station %/% 1 == 4500)
 
 df.lastspeedgrade5.0 <- tmpdf.lastspeedgrade5.0[!duplicated(tmpdf.lastspeedgrade5.0$Axles),]
 
@@ -64,13 +64,13 @@ df.lastspeedgrade5.0
 df.expectedspeed60pw6.0grade5.5 <- df.pw[df.pw$Grade == "5.5",]
 
 plot.expectedspeed60pw6.0grade5.5 <- ggplot(df.expectedspeed60pw6.0grade5.5,
-                                            aes(x = Station, y = Speed))+
-  geom_line(aes(colour = factor(Axles)), size = 1.2)+
-  geom_hline(yintercept = 60, linetype = "dashed", color = "red", size = 1.0)+
-  scale_x_continuous(limits = c(0, 5000), breaks = seq(0, 5000, 500))+
-  scale_y_continuous(limits = c(0, 65), breaks = seq(0, 65, 10))+
-  scale_colour_hue("轴数")+
-  labs(title = "冲坡速度60km/h, 比功率6.0kW/T，坡度5.5%", x = "位置", y = "速度, km/h")+
+                                            aes(x = Station, y = Speed)) +
+  geom_line(aes(colour = factor(Axles)), size = 1.2) +
+  geom_hline(yintercept = 60, linetype = "dashed", color = "red", size = 1.0) +
+  scale_x_continuous(limits = c(0, 5000), breaks = seq(0, 5000, 500)) +
+  scale_y_continuous(limits = c(0, 65), breaks = seq(0, 65, 10)) +
+  scale_colour_hue("轴数") +
+  labs(title = "冲坡速度60km/h, 比功率6.0kW/T，坡度5.5%", x = "位置", y = "速度, km/h") +
   theme(legend.title = element_text(face = "bold", size = 10),
         legend.text = element_text(face = "bold", size = 10),
         axis.title = element_text(face = "bold", size = 11),
@@ -81,11 +81,9 @@ plot.expectedspeed60pw6.0grade5.5
 
 # 冲破速度60，比功率6.0，坡度5.5%，不同轴数货车稳定速度----
 tmpdf.lastspeedgrade5.5 <- subset(df.expectedspeed60pw6.0grade5.5,
-                                  df.expectedspeed60pw6.0grade5.5$Station%/%1 == 4500)
+                                  df.expectedspeed60pw6.0grade5.5$Station %/% 1 == 4500)
 
 df.lastspeedgrade5.5 <- tmpdf.lastspeedgrade5.5[!duplicated(tmpdf.lastspeedgrade5.5$Axles),]
 
 df.lastspeedgrade5.5
-
-
 
